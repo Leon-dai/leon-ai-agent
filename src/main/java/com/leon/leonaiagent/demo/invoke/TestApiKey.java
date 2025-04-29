c4fd51f2e621ad3896641942353952bc6283484b
leon <212649354@qq.com>
2025-04-30 00:10:08 +0800

    1.1 隐藏了API_KEY

--- src/main/java/com/leon/leonaiagent/demo/invoke/TestApiKey.java
+++ src/main/java/com/leon/leonaiagent/demo/invoke/TestApiKey.java
@@ -4,5 +4,6 @@ package com.leon.leonaiagent.demo.invoke;
  * 仅用于测试获取API Key
  */
 public interface TestApiKey {
-    String API_KEY = "sk-f17559045ebd400f8ff7f070c52d4d3e";
+    // 这里做了修改
+    String API_KEY = "1234";
 }
