656ebed9a79aa70a1f2c82ac5b29bd8307f9aea2
leon <212649354@qq.com>
2025-06-02 21:41:06 +0800

    2.AI应用开发-多轮对话AI应用开发

---
+++ src/test/java/com/leon/leonaiagent/app/LoveAppTest.java
@@ -0,0 +1,31 @@
+package com.leon.leonaiagent.app;
+
+import cn.hutool.core.lang.UUID;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveAppTest {
+
+    @Resource
+    private LoveApp loveApp;
+
+    @Test
+    void testChat() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是leon";
+        String answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第二轮
+        message = "我想让另一半（xyx）更爱我";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第三轮
+        message = "我的另一半叫什么来着？刚跟你说过，帮我回忆一下";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+}
